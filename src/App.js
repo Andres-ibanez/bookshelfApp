9a9a2f5cf138fe5dfd66a8be03bbf7cdbd866340
Andres <cristian.ibanez@aluno.unila.edu.br>
2020-07-10 22:50:12 -0300

    Actualizando el index con implementacion de BrowserRoute, Route, Link.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Header from './components/Header'
+import BookShelf from './components/BookShelf'
+import AddBook from './components/AddBook'
+import { Link, Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
+    state = {
+        categorys: {
+            currentlyReading: 'Currently Reading',
+            wantToRead: 'Want to Read',
+            read: 'Read'
+        },
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then(books => {
+                this.setState(() => ({
+                    books
+                }))
+            })
+    }
+
+    updateBooks = (bookUpdate, shelfUpdate) => {
+        (bookUpdate.shelf === 'none') ?
+            this.setState(currentState => ({
+                books: [...currentState.books, { ...bookUpdate, shelf: shelfUpdate }]
+            }))
+            :
+            this.setState(currentState => ({
+                books: currentState.books.map(book => book.title === bookUpdate.title ? { ...book, shelf: shelfUpdate } : book)
+            }))
+        //console.log(this.state.books, 'update', bookUpdate)
+        //Atualizando os dados usando metodo da API
+        BooksAPI.update(bookUpdate, shelfUpdate)
+    }
 
     render() {
         return (
             <div className="app">
-                <div className="list-books">
-                    <Header />
-                    <div className="list-books-content">
-
+            Route perfime informar rota, componente, etc.
+                <Route exact path='/' render={() => (
+                    <div className="list-books">
+                        <Header />
+                        <div className="list-books-content">
+                            <div>
+                                <BookShelf books={this.state.books} categorys={this.state.categorys} update={this.updateBooks} />
+                            </div>
+                        </div>
+                        <div className="open-search">
+                        {/* etiquetas <a> nesecitan ser substituidas por Link para 
+                        trabajar adecuadamente con BrowserRoute, Route */}
+                            <Link to='/addBook'>Add a book</Link>
+                        </div>
                     </div>
-                </div>
+                )}
+                />
+                <Route path='/addbook' render={() => (
+                    <AddBook update={this.updateBooks} books={this.state.books} />
+                )} />
+
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import BooksApp from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BooksApp />,
-  document.getElementById('root')
-);
-
+/* BrowserRouter indica onde teremos roteamento de componentes para importar ele 
+precisamos instalar o react-router-dom */
+  <BrowserRouter>
+  	<App />
+  </BrowserRouter>, document.getElementById('root'))
